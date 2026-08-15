ccd5a57c12ab98888cf73ef59427f8b32a098345
OliOli2013 <pawelzarzycki61@gmail.com>
2026-08-15 19:55:23 +0200

    Aktualizacja

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-/* AIO-IPTV.pl PWA — AIO Panel 16.0.0 + IPTV Dream 8.0.0 + NeoRadio 3.0.0 + mobile header fix • 2026-08-15 */
-const CACHE='aio-iptv-pro-20260815-mobile-header1';
+/* AIO-IPTV.pl PWA — AIO Panel 16.0.0 + IPTV Dream 8.0.0 + NeoRadio 3.0.0 + Nagrania On Demand 3.0.0 + mobile header fix • 2026-08-15 */
+const CACHE='aio-iptv-pro-20260815-nagrania300';
 const CORE=[
   './assets/js/auto-language.js?v=20260729-auto-en1',
   './','./index.html','./downloads.html','./plugins.html','./guides.html','./systems.html',
